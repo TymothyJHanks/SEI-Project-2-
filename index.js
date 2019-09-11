b58f4e9f4f70406c4173d44fab1d42981e660cb2
Tymothy J Hanks <tymjhanks@gmail.com>
2019-09-11 16:16:12 -0400

    worked on index.js file

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const express = ("express");
+const express = require('express');
 const app = express();
+const parser = require("body-parser");
+const charactersRoutes = require("./routes/charactersRoutes");
+const locationsRoutes = require("./routes/locationsRoutes");
+const episodesRoutes = require("./routes/episodesRoutes");
+
+app.use(parser.urlencoded({extended: true})); //interperts keyy value pairs in URLS
+app.use(parser.json()); //converts a json string to the obect and attaches it to req.body
+
+app.use("/api/characters/", charactersRoutes);
+app.use("/api/locations/", locationsRoutes);
+app.use("/api/episodes/", episodesRoutes);
+
+app.listen(8000, () => console.log("Everything is working on port 8000"))
+
+
+
