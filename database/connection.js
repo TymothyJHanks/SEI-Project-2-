31b646c1ca6e53d47c1ca2840509ef976e4c107b
Tymothy J Hanks <tymjhanks@gmail.com>
2019-09-11 16:04:30 -0400

    added seed.js file and working on it

---
+++ database/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = Promise
+
+const mongoURI = 'mongodb://localhost/rick-and-morty'
+
+mongoose
+.connect(mongoURI, { useNewUrlParser: true })
+.then((conn) => {
+	console.log(`connected to mongodb on ${conn.connections[0].name} db`)
+})
+.catch(err => {
+	console.error(err)
+})
+
+
+module.exports = mongoose
